9884a6fd59ce4e78e4fced74031cb1c2bcabefea
Sean Bannon <seannbannon@gmail.com>
2022-02-10 17:47:04 -0600

    added rick roll and formatted social media buttons oh yeah

--- portfolio/src/PortfolioContainer/Home/Profile.js
+++ portfolio/src/PortfolioContainer/Home/Profile.js
@@ -13,12 +13,9 @@ export default function Profile() {
                                 <i className='fa fa-facebook-square'></i>
                             </a>
                             <a href='#'>
-                                <i className='fa fa-google-plus-square'></i>
-                            </a>
-                            <a href='#'>
                                 <i className='fa fa-instagram'></i>
                             </a>
-                            <a href='#'>
+                            <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
                                 <i className='fa fa-youtube-square'></i>
                             </a>
                             <a href='#'>
