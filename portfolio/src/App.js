77490db3b6aae275b6ba4070ec064031f9d81d14
Sean Bannon <seannbannon@gmail.com>
2022-02-12 14:47:51 -0600

    footer lookin good

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,10 @@
-import Profile from './PortfolioContainer/Home/Profile';
 import './App.css';
-import Footer from './PortfolioContainer/Home/Footer/Footer';
+import Home from './PortfolioContainer/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Profile />
-      <Footer/>
+      <Home />
     </div>
   );
 }
