d4a4df2d9ef10eb6492b7523199c336ad5185b1d
Sean Bannon <seannbannon@gmail.com>
2022-02-12 14:39:09 -0600

    footer css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import Profile from './PortfolioContainer/Home/Profile';
 import './App.css';
+import Footer from './PortfolioContainer/Home/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Footer/>
     </div>
   );
 }
