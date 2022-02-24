e055a4514d556653ed754720831914f9198c261f
Sean Bannon <seannbannon@gmail.com>
2022-02-24 13:09:42 -0600

    cleaning up code, combing through to try to find why hamburger is not working

--- portfolio/src/PortfolioContainer/Home/Header/Header.js
+++ portfolio/src/PortfolioContainer/Home/Header/Header.js
@@ -7,28 +7,29 @@ import './Header.css'
 
 
 export default function Header() {
-    const [selectedScreen,setSelectedScreen] = useState(0)
-    const [showHeaderOptions, setShowHeaderOptions] = useState(false)
+    const [selectedScreen,setSelectedScreen] = useState(0);
+    const [showHeaderOptions, setShowHeaderOptions] = useState(false);
 
     const updateCurrentScreen = (currentScreen) => {
         if (currentScreen || !currentScreen.screenInView)
         return;
-        let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView)
+        let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
         if(screenIndex < 0)
-        return
+        return;
     }
-    let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen)
+    let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
 
     const getHeaderOptions = () => {
-        return(
-            TOTAL_SCREENS.map((screen, i) => (
-                <div key = {screen.screen_name} className = {getHeaderOptionsClass(i)}
-                onClick = {() => switchScreen(i, screen)}>
+        return TOTAL_SCREENS.map((screen, i) => (
+                <div 
+                    key = {screen.screen_name} 
+                    className = {getHeaderOptionsClass(i)}
+                    onClick = {() => switchScreen(i, screen)}
+                >
                     <span>{screen.screen_name}</span>
                 </div>
-            ))
-        )
-    }
+        ));
+    };
 
     const getHeaderOptionsClass = (index) => {
         let classes = "header-options";
@@ -37,23 +38,28 @@ export default function Header() {
 
         if (selectedScreen === index)
         classes += "selected-header-options";
-        return
-    }
+        return classes;
+    };
 
-    const switchScreen = (index,screen) => {
-        let screenComponent = document.getElementById(screen.screen_name)
+    const switchScreen = (index, screen) => {
+        let screenComponent = document.getElementById(screen.screen_name);
         if(!screenComponent)
-        return
+        return;
 
-        screenComponent.scrolllIntoView({behavior: 'smooth'})
+        screenComponent.scrolllIntoView({behavior: 'smooth'});
         setSelectedScreen(index);
-        setShowHeaderOptions(false)
+        setShowHeaderOptions(false);
     };
 
 
 
+    // useEffect(() => {
+    //     return () => {
+    //       currentScreenSubscription.unsubscribe();
+    //     };
+    //   }, [currentScreenSubscription]);
+    
     return (
-    <div>
         <div 
         className='header-container' 
         onClick = {() => setShowHeaderOptions(!showHeaderOptions)}
@@ -61,22 +67,113 @@ export default function Header() {
             <div className='header-parent'>
                 <div 
                     className='header-hamburger' 
-                    onClick = {() => setShowHeaderOptions(!showHeaderOptions)}
+                    onClick ={() => setShowHeaderOptions(!showHeaderOptions)}
                 >
                     <FontAwesomeIcon className='header-hamburger-bars' icon = {faBars}/>
                 </div>
                 <div className='header-logo'>
-                    <span>SEAN~</span>
+                    <span>SEAN</span>
                 </div>
-                <div className={
-                showHeaderOptions
-                ? "header-options show-hamburger-options"
-                : "header-options"
-                }>
+                <div 
+                    className={
+                        showHeaderOptions
+                            ? "header-options show-hamburger-options"
+                            : "header-options"
+                    }
+                >
                     {getHeaderOptions()}
                 </div>
             </div>
         </div>
-    </div>
   )
 }
+
+
+
+// import React, { useState, useEffect } from "react";
+// import {
+//   TOTAL_SCREENS,
+//   GET_SCREEN_INDEX,
+// } from "../../../utilities/commonUtils";
+// import ScrollService from "../../../utilities/ScrollService";
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import "./Header.css";
+
+// export default function Header() {
+//   const [selectedScreen, setSelectedScreen] = useState(0);
+//   const [showHeaderOptions, setShowHeaderOptions] = useState(false);
+
+//   const updateCurrentScreen = (currentScreen) => {
+//     if (!currentScreen || !currentScreen.screenInView) return;
+
+//     let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
+//     if (screenIndex < 0) return;
+//   };
+//   let currentScreenSubscription =
+//     ScrollService.currentScreenBroadcaster.subscribe(updateCurrentScreen);
+
+//   const getHeaderOptions = () => {
+//     return TOTAL_SCREENS.map((Screen, i) => (
+//       <div
+//         key={Screen.screen_name}
+//         className={getHeaderOptionsClasses(i)}
+//         onClick={() => switchScreen(i, Screen)}
+//       >
+//         <span>{Screen.screen_name}</span>
+//       </div>
+//     ));
+//   };
+
+//   const getHeaderOptionsClasses = (index) => {
+//     let classes = "header-option ";
+//     if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator ";
+
+//     if (selectedScreen === index) classes += "selected-header-option ";
+
+//     return classes;
+//   };
+
+//   const switchScreen = (index, screen) => {
+//     let screenComponent = document.getElementById(screen.screen_name);
+//     if (!screenComponent) return;
+
+//     screenComponent.scrollIntoView({ behavior: "smooth" });
+//     setSelectedScreen(index);
+//     setShowHeaderOptions(false);
+//   };
+
+//   useEffect(() => {
+//     return () => {
+//       currentScreenSubscription.unsubscribe();
+//     };
+//   }, [currentScreenSubscription]);
+
+//   return (
+//     <div
+//       className="header-container"
+//       onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+//     >
+//       <div className="header-parent">
+//         <div
+//           className="header-hamburger"
+//           onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+//         >
+//           <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+//         </div>
+//         <div className="header-logo">
+//           <span>EDUBABA</span>
+//         </div>
+//         <div
+//           className={
+//             showHeaderOptions
+//               ? "header-options show-hamburger-options"
+//               : "header-options"
+//           }
+//         >
+//           {getHeaderOptions()}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
