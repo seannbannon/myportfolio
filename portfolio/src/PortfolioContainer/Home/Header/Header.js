b2fc6e954c2feb0cd9ec281ccabf664c1b65fe0c
Sean Bannon <seannbannon@gmail.com>
2022-02-26 22:15:12 -0600

    still can not figure out why this dang header wont work wtf

--- portfolio/src/PortfolioContainer/Home/Header/Header.js
+++ portfolio/src/PortfolioContainer/Home/Header/Header.js
@@ -51,8 +51,6 @@ export default function Header() {
         setShowHeaderOptions(false);
     };
 
-
-
     // useEffect(() => {
     //     return () => {
     //       currentScreenSubscription.unsubscribe();
