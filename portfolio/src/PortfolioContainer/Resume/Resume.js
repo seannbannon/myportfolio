34717f4e8fc82713f22d940e16fedd8b0f80f63e
Sean Bannon <seannbannon@gmail.com>
2022-03-05 11:15:02 -0600

    adding projects and programming skills and resume bullets

--- portfolio/src/PortfolioContainer/Resume/Resume.js
+++ portfolio/src/PortfolioContainer/Resume/Resume.js
@@ -29,7 +29,47 @@ export default function Resume(props) {
                 </div>
             </div>
         </div>
-    }
+    };
+
+    const resumeBullets = [
+        {label: "Education", logoSrc: "education.svg"},
+        {label: "Work History", logoSrc: "work-history.svg"},
+        {label: "Programming Skills", logoSrc: "programming-skills.svg"},
+        {label: "Projects", logoSrc: "projects.svg"},
+        {label: "Interests", logoSrc: "interests.svg"}
+    ];
+
+    const programmingSkillDetails = [
+        {skill: "JavaScript", ratingPercentage: 85},
+        {skill: "React JS", ratingPercentage: 85},
+        {skill: "React Native", ratingPercentage: 85},
+        {skill: "Express JS", ratingPercentage: 70},
+        {skill: "Node JS", ratingPercentage: 89},
+        {skill: "Mongo DB", ratingPercentage: 70},
+        {skill: "HTML", ratingPercentage: 85},
+        {skill: "CSS", ratingPercentage: 85},
+    ];
+
+    const projectDetails = [
+        {
+            title: "Personal Portfolio Website",
+            duration: {fromDate: "2021", toDate:"2022"},
+            description: "A Personal Portfolio website to shocase all my details and projects in one place.",
+            subHeading: "Technologies Used: ReactJS, Bootstrap, RxJS"
+        },
+        {
+            title: "Filler in L8r",
+            duration: {fromDate: "2021", toDate:"2022"},
+            description: "This is filler words, la la la",
+            subHeading: "Technologies Used: ReactJS, Bootstrap, RxJS"
+        },
+        {
+            title: "Blah Blah",
+            duration: {fromDate: "2021", toDate:"2022"},
+            description: "One time when I was little an older man tried to get me to help him find his puppy.",
+            subHeading: "Technologies Used: ReactJS, Bootstrap, RxJS"
+        },
+    ];
 
     let fadeInScreenHandler = (screen) => {
         if(screen.fadeScreen !== props.id)
