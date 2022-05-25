84bd19984238fc9eb266e55b78bf01e47d767ea7
Sean Bannon <seannbannon@gmail.com>
2022-05-25 13:18:35 -0500

    make it better

--- server.js
+++ server.js
@@ -12,5 +12,12 @@ app.use(cors());
 
 app.use("/", contactRoute);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+}
+
 const port = process.env.PORT || 4000;
 app.listen(port, console.log(`Server Listening To Port 4000`));
